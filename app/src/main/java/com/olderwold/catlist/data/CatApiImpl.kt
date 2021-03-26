d6ebf964f2d5ff6ff955c1b6248abf08a787accd
Tom Koptel <tom.koptel@gmail.com>
2021-03-26 15:05:04 +0100

    Setting up the initial network API impl

--- app/src/main/java/com/olderwold/catlist/data/CatApiImpl.kt
+++ app/src/main/java/com/olderwold/catlist/data/CatApiImpl.kt
@@ -3,9 +3,47 @@ package com.olderwold.catlist.data
 import com.olderwold.catlist.domain.Cat
 import com.olderwold.catlist.domain.CatApi
 import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-class CatApiImpl : CatApi {
+class CatApiImpl private constructor(
+    private val api: Api
+) : CatApi {
+
+    companion object Factory {
+        operator fun invoke(block: OkHttpClient.Builder.() -> Unit = {}): CatApi {
+            val client = OkHttpClient.Builder()
+                .apply(block)
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+                .build()
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.thecatapi.com")
+                .build()
+            val api = retrofit.create(Api::class.java)
+            return CatApiImpl(api)
+        }
+    }
+
     override fun catList(): Single<List<Cat>> {
-        TODO("Not yet implemented")
+        return api.list(limit = 5)
+            .map { listOf<Cat>() }
+    }
+
+    // https://api.thecatapi.com/v1/images/search?limit=5
+    private interface Api {
+        @GET("/v1/images/search")
+        fun list(@Query("limit") limit: Int): Single<Response<ResponseBody>>
     }
 }
--- app/src/test/java/com/olderwold/catlist/data/CatApiImplTest.kt
+++ app/src/test/java/com/olderwold/catlist/data/CatApiImplTest.kt
@@ -1,11 +1,26 @@
 package com.olderwold.catlist.data
 
+import okreplay.*
+import org.junit.Rule
 import org.junit.Test
 
 class CatApiImplTest {
+    private val okReplayInterceptors = OkReplayInterceptor()
+    private val okReplayConfig = OkReplayConfig.Builder()
+        .interceptor(okReplayInterceptors)
+        .defaultMode(TapeMode.WRITE_ONLY)
+        .build()
+
+    private val catApi = CatApiImpl {
+        addInterceptor(okReplayInterceptors)
+    }
+
+    @get:Rule
+    val rule = RecorderRule(okReplayConfig)
+
+    @OkReplay(tape = "happy_case_cat_list")
     @Test
     fun `provide list of cats happy case`() {
-        val catApiImpl = CatApiImpl()
-        catApiImpl.catList().test().assertNoErrors()
+        catApi.catList().test().assertNoErrors()
     }
 }
