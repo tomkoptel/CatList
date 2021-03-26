77c7deadbb060132937ae26a5787aa324aab2e73
Tom Koptel <tom.koptel@gmail.com>
2021-03-26 15:50:00 +0100

    Setting up cat_item + linking to CatListAdapter.kt

--- app/src/main/java/com/olderwold/catlist/ui/CatListAdapter.kt
+++ app/src/main/java/com/olderwold/catlist/ui/CatListAdapter.kt
@@ -1,23 +1,39 @@
 package com.olderwold.catlist.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.olderwold.catlist.R
+import com.olderwold.catlist.domain.Cat
 
 class CatListAdapter : RecyclerView.Adapter<CatListAdapter.ViewHolder>() {
+    var data: List<Cat> = emptyList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val layout = inflater.inflate(R.layout.cat_item, parent, false)
+        return ViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val cat = data[position]
+        holder.bind(cat)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = data.size
 
     class ViewHolder(
-        private val itemView: View
-    ) : RecyclerView.ViewHolder(itemView) {}
+        layout: View
+    ) : RecyclerView.ViewHolder(layout) {
+        private val imageView = layout.findViewById<ImageView>(R.id.imageView)
+
+        fun bind(item: Cat) {
+            // Glide to load image
+        }
+
+        fun recycle() {
+        }
+    }
 }
