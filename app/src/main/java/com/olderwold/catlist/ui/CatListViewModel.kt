c0b983ca645c319b42aee8136a11054930dcd294
Tom Koptel <tom.koptel@gmail.com>
2021-03-26 15:44:22 +0100

    CatListViewModel always happy :)

--- app/src/main/java/com/olderwold/catlist/ui/CatListViewModel.kt
+++ app/src/main/java/com/olderwold/catlist/ui/CatListViewModel.kt
@@ -1,6 +1,33 @@
 package com.olderwold.catlist.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.olderwold.catlist.domain.Cat
+import com.olderwold.catlist.domain.CatApi
 
-class CatListViewModel : ViewModel() {
+class CatListViewModel(
+    private val catApi: CatApi,
+) : ViewModel() {
+    private val _state: MutableLiveData<State> = MutableLiveData<State>()
+
+    val state: LiveData<State> = _state
+
+    fun load() {
+        _state.value = State.Success(
+            listOf(Cat(id = "7n2", url = "https://cdn2.thecatapi.com/images/7n2.jpg"))
+        )
+    }
+
+    sealed class State {
+        data class Error(
+            val value: Throwable
+        ): State()
+
+        object Loading: State()
+
+        data class Success(
+            val catList: List<Cat>
+        ): State()
+    }
 }
